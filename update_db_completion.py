f478ba3c2e48c34709bcc822ab9f2b5b0ca3b91b
zebus3d <zebus3dream@gmail.com>
2012-07-05 14:32:04 +0200

    update db completion

---
+++ update_db_completion.py
@@ -0,0 +1,7 @@
+import sys, shelve
+# devolvemos listado de completion_items
+def completion_items():
+    clientes_db = shelve.open("clientes.db")
+    completion_items = [clientes_db.keys()]
+    clientes_db.close()
+    return completion_items
