7baf470a494d2a6683c2fa091863a2c34c218f82
damasko <damasko015@yahoo.es>
2012-07-06 17:42:39 +0200

    reemplazo update nueva interfaz

--- reemplazo_para_interfaz.sh
+++ reemplazo_para_interfaz.sh
@@ -2,4 +2,4 @@
 
 rm interfaz.py ; pyuic4 interfaz_v005.ui -o interfaz.py
 
-sed -i s/"from PyQt4 import QtCore, QtGui"/"from PyQt4 import QtCore, QtGui \nfrom completion import *"/g interfaz.py ; sed -i s/"self.namec = QtGui.QLineEdit(self.gridLayoutWidget_2)"/"self.namec = miQLineEdit(self.gridLayoutWidget_2)"/g interfaz.py
+sed -i s/"from PyQt4 import QtCore, QtGui"/"from PyQt4 import QtCore, QtGui \nfrom completion import *"/g interfaz.py ; sed -i s/"self.namec = QtGui.QLineEdit(self.centralwidget)"/"self.namec = miQLineEdit(self.centralwidget)"/g interfaz.py
